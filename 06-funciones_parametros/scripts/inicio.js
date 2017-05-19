f73812de151fd1fb9ae184a6497a1c836ea13a79
alce65 <alce65@hotmail.es>
2017-05-19 17:31:16 +0200

    funciones

--- 06-funciones_parametros/scripts/inicio.js
+++ 06-funciones_parametros/scripts/inicio.js
@@ -14,6 +14,13 @@ function sumar (a=0, b=0, c=0, d=0) {
     return a+b+c+d;
 }
 
+
+function sumarDobles (a=0, b=0) {
+    a *= 2; // a =  2*a
+    b *= 2;
+    return a+b;
+}
+
 function sumarArg () {
     let nResult = 0;
     for (var i = 0; i < arguments.length; i++) {
@@ -45,6 +52,10 @@ function main () {
     console.log(sumarArg (nNum1, nNum2, nNum3, nNum4, 12, 12));    
     console.log(sumarArg (12, 23, 12, 23, 45, 32, 12, 54, 32, 65, 76 ));
     console.log(media (12, 12, 12, 12, 12, 12, 12, 12, 12, 12 ));
+    console.log(sumarDobles(nNum1, nNum2))
+
+    console.log(nNum1)
+    console.log(nNum2)
     
     saludar();
     
